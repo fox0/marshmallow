2a9e4a038396c16c4a8705724a83c0a917af2810
fox0 <red-fox0@yandex.ru>
2019-02-07 10:43:05 +0700

    *minor

--- marshmallow.py
+++ marshmallow.py
@@ -1,5 +1,7 @@
 import os.path
+import time
 import logging
+from collections import namedtuple
 from multiprocessing import Process, Queue, current_process, freeze_support, cpu_count
 
 from lupa import LuaRuntime, LuaError
@@ -10,23 +12,47 @@ log = logging.getLogger('marshmallow')
 
 DEBUG = True
 
+MyTask = namedtuple('MyTask', ('func', 'args'))
 
-def worker(q_in, q_out):
-    for func, args in iter(q_in.get, None):
+
+def process_worker(q_in: Queue, q_out: Queue):
+    for func, args in iter(q_in.get, None):  # MyTask
+        # watch_dog.start()
+        result = None
         try:
             log.debug('%s run %s%s', current_process().name, func.__name__, args)
-            # tmp = threading.Thread(target=do_work, args=(id, lambda: stop_threads))
-            result = func(*args)  # todo timeout
-            q_out.put(result)
+            result = func(*args)
         except BaseException:
             log.exception('error:')
-            q_out.put(None)
+        q_out.put(result)
+        # watch_dog.stop()
+
+
+def run_workers(q_in, q_out):
+    number_cpu = cpu_count()
+    log.debug('running %d process…', number_cpu)
+    for i in range(number_cpu):
+        Process(target=process_worker, args=(q_in, q_out)).start()
 
 
 def task_read_pattern(filename):
     fn = os.path.join('patterns', filename + '.lua')
     with open(fn) as f:
         return f.read()
+
+
+def load_patterns(q_in, q_out):
+    result = []
+    log.debug('load patterns…')
+    patterns = ('test', 'test', 'test', 'test', 'test',)
+    for i in patterns:
+        q_in.put(MyTask(task_read_pattern, (i,)))  # todo delme
+    for _ in range(len(patterns)):
+        r = q_out.get()
+        if r is not None:
+            result.append(r)
+    assert len(result) > 0
+    return result
 
 
 def task_task(lua_code):
@@ -40,30 +66,19 @@ def task_task(lua_code):
 
 
 def main():
-    number_cpu = cpu_count()
-    log.debug('running %d process…', number_cpu)
     q_in, q_out = Queue(), Queue()
-    for i in range(number_cpu):
-        Process(target=worker, args=(q_in, q_out)).start()
+    run_workers(q_in, q_out)
+    codes = load_patterns(q_in, q_out)
 
-    log.debug('load patterns…')
-    patterns = ('test', 'test', 'test', 'test', 'test',)
-    for i in patterns:
-        q_in.put((task_read_pattern, (i,)))  # todo delme
-    codes = []
-    for _ in range(len(patterns)):
-        r = q_out.get()
-        if r is not None:
-            codes.append(r)
-
-    log.debug('running…')
+    log.debug('running patterns…')
     for i in codes:
-        q_in.put((task_task, (i,)))
+        q_in.put(MyTask(task_task, (i,)))
 
     for _ in range(len(codes)):
         print(q_out.get())
 
     log.debug('shutdown…')
+    number_cpu = cpu_count()  # todo
     for i in range(number_cpu):
         q_in.put(None)
 
